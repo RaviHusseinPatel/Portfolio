d5b7982423d8f1ff33bb7a0a56bc4501226a73a7
Ravi husseinpatel <ravi@ravis-air-2.home>
2022-12-11 00:13:26 +0000

    add swiper JS elements to Testimonials

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import "./header.css"
 import CTA from "./CTA";
 import dp from '../../assets/dp.png'
+import dp1 from '../../assets/dp.png'
+
 import HeaderSocials from "./HeaderSocials";
 
 
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,9 +1,66 @@
 import "./testimonials.css"
+import AVTR1 from "../../assets/AVTR1.jpg"
+import AVTR2 from "../../assets/AVTR2.jpg"
+import AVTR3 from "../../assets/AVTR3.jpg"
+import AVTR4 from "../../assets/AVTR4.jpg"
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination  } from "swiper"
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const data= [
+  {
+    avatar: AVTR1,
+  name:'Anna Henderson',
+  review:"lorem ipsum.............."
+},
+  {
+  avatar: AVTR2,
+  name:'Yemi Mcewen',
+  review:"lorem ipsum.............."
+  },
+  {
+  avatar: AVTR3,
+  name:'Zsolt Podoba-Szalai',
+  review:"lorem ipsum.............."
+  },
+  {
+  avatar: AVTR4,
+  name:'Sadik Tekin ',
+  review:"lorem ipsum.............."
+  },
+]
 
 const Testimonials = () => {
     return (
       <section id="testimonials">
-       Testimonials
+        <h5>Colleague/Collaborator reviews</h5>
+       <h2>Testimonials</h2>
+
+       <Swiper className="container testimonials__container"
+
+       modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}>
+        {
+          data.map(({avatar, name, review},index)=>{
+            return (
+              <SwiperSlide key={index} className="testimonial">
+                <div className="collab__avatar">
+                  <img src={avatar}/>
+                </div>
+                <h5 className="collab__name">{name}</h5>
+                <small className="collab__review">{review}</small>
+              </SwiperSlide>
+            )
+          })
+        }
+       </Swiper>
       </section>
     )
   };
