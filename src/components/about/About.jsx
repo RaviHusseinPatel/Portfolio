e145b5987a24683678d101d46c6747bb276999c9
Ravi Hussein-Patel <109157608+RaviHusseinPatel@users.noreply.github.com>
2022-12-16 12:33:38 +0000

    Add first svg waves layer

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,56 +3,58 @@ import dp from '../../assets/dp.png'
 import {FaAward} from 'react-icons/fa'
 import {FiUsers} from 'react-icons/fi'
 import {BiGitBranch} from 'react-icons/bi'
+import layer1 from '../../assets/layer1.svg'
 
 const About = () => {
     return (
-      <section id="about">
-       <h5> A little </h5>
-       <h2>About Me & Why I program</h2>
+      <section id="about" className="space layer1">
+          <img src={layer1}/>
+         
+        <h5> A little </h5>
+        <h2>About Me & Why I program</h2>
 
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src={dp} alt="About Image"/>
+        <div className="container about__container">
+          <div className="about__me">
+            <div className="about__me-image">
+              <img src={dp} alt="About Image"/>
+            </div>
+          </div>
+
+          <div className="about__content">
+            <div className="about__cards">
+              <article className="about__card">
+                <FaAward className="about_icon"/>
+                <h5>Experience</h5>
+                <small>1+ Years</small>
+              </article>
+
+              <article className="about__card">
+                <FiUsers className="about_icon"/>
+                <h5>Clients</h5>
+                <small>20+ Worldwide</small>
+              </article>
+
+              <article className="about__card">
+                <BiGitBranch className="about_icon"/>
+                <h5>Projects</h5>
+                <small>25+ Completed</small>
+              </article>
+            </div>
+            <p>
+            As an aspiring React developer with a year of experience building backend and frontend APIs, I have strong communication skills and a deep understanding of how to work with others. My experience in client-facing roles and as a tutor has given me the ability to ask the right questions and learn from others, which is a crucial skill for any developer.
+            I was able to demonstrate these skills during the BrightNetwork Technology Academy, where our group's final projects received recognition for being both creative and efficient. <br/> <br/>
+                </p>
+                <p className="why">
+                Programming is important because it allows us to harness the power of engineering to improve the lives of people on a daily basis. 
+                This is what motivates me to code. The ability to be vreative and collaborate with like-minded individuals from around the world to solve complex problems using technology however, is what excites me about this field.
+                </p>
+                
+            <a href="#contact" className="btn btn-primary" id="reachout">Reach Out</a>
           </div>
         </div>
-
-        <div className="about__content">
-          <div className="about__cards">
-            <article className="about__card">
-              <FaAward className="about_icon"/>
-              <h5>Experience</h5>
-              <small>1+ Years</small>
-            </article>
-
-            <article className="about__card">
-              <FiUsers className="about_icon"/>
-              <h5>Clients</h5>
-              <small>20+ Worldwide</small>
-            </article>
-
-            <article className="about__card">
-              <BiGitBranch className="about_icon"/>
-              <h5>Projects</h5>
-              <small>25+ Completed</small>
-            </article>
-          </div>
-          <p>
-          As an aspiring React developer with a year of experience building backend and frontend APIs, I have strong communication skills and a deep understanding of how to work with others. My experience in client-facing roles and as a tutor has given me the ability to ask the right questions and learn from others, which is a crucial skill for any developer.
-          I was able to demonstrate these skills during the BrightNetwork Technology Academy, where our group's final projects received recognition for being both creative and efficient. <br/> <br/>
-              </p>
-              <p className="why">
-              Programming is important because it allows us to harness the power of engineering to improve the lives of people on a daily basis. 
-              This is what motivates me to code. The ability to be vreative and collaborate with like-minded individuals from around the world to solve complex problems using technology however, is what excites me about this field.
-              </p>
-              
-
-
-          <a href="#contact" className="btn btn-primary" id="reachout">Reach Out</a>
-        </div>
-      </div>
-      
+        <img src={layer1}/>
       </section>
+      
     )
   };
   
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -22,7 +22,7 @@ const data= [
   avatar: AVTR2,
   name:<a href="https://www.linkedin.com/in/yemi-m-30700141/" target="_blank" >Yemi Mcewen</a>,
   title:'Senior Software Engineer, Tax Systems',
-  review:"lorem ipsum.............."
+  review:"I have been working with Ravi for the past few months on several web development projects, and I cannot say enough good things about him. He has helped me design and build both a WordPress website and a React-based web app, and I have been consistently impressed with his ability to pick up knowledge quickly, skills, and attention to detail."
   },
   {
   avatar: AVTR3,
@@ -41,7 +41,7 @@ const data= [
   name:<a href="https://www.linkedin.com/in/stekin/" target="_blank" >Suraaj Leal</a>,
   title:'Software Engineer, ATOS',
   
-  review:"As a Bright Network Academy Mentor and now Engineer at Atos, I've had the pleasure of working with Ravi on several projects. His communication skills are top-notch and is always able to clearly articulate his thoughts and ideas. Additionally, they have a strong ability to learn and ask the right questions, which has helped to ensure the success of our projects. I highly recommend this engineer for any team."
+  review:"As a Bright Network Academy Mentor and now Engineer at Atos, I've had the pleasure of working with Ravi on several projects. His communication skills are top-notch and is always able to clearly articulate his thoughts and ideas. Additionally, he has a strong ability to learn and ask the right questions, which has helped to ensure the success of our projects. I highly recommend this engineer for any team."
   },
 ]
 
@@ -49,15 +49,16 @@ const Testimonials = () => {
     return (
       <section id="testimonials">
         <h5>Colleague/Collaborator reviews</h5>
-       <h2>Testimonials</h2>
+        <h2>Testimonials</h2>
 
        <Swiper className="container testimonials__container"
 
-       modules={[Pagination]}
-      spaceBetween={40}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}>
+        modules={[Pagination]}
+        spaceBetween={40}
+        slidesPerView={1}
+        navigation
+        pagination={{ clickable: true }}>
+
         {
           data.map(({avatar, name, title, review},index)=>{
             return (
